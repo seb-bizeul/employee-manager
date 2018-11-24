71ed3a6f768ad2ed50338e14b54fd7ef2f0e05a7
Seb Bizeul <seb.bizeul@gmail.com>
2018-11-24 10:25:38 +0100

    build: add redux devtools

--- src/modules/app/configureStore.js
+++ src/modules/app/configureStore.js
@@ -1,16 +1,23 @@
 // @flow strict
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootSaga from './saga'
 import rootReducer from './reducer'
 
 const sagaMiddleware = createSagaMiddleware()
 
+const middlewares = [
+  sagaMiddleware
+]
+
 export default function configureStore() {
   const store = createStore(
     rootReducer,
-    applyMiddleware(sagaMiddleware)
+    composeWithDevTools(
+      applyMiddleware(...middlewares)
+    )
   )
   sagaMiddleware.run(rootSaga)
   return store
