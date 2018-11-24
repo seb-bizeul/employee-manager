fbd6024f9c640bb3e2d9cabd3b20d5af9c3900c6
Seb Bizeul <seb.bizeul@gmail.com>
2018-11-24 12:26:52 +0100

    refactor(helpers): use native currying instead of R.partial

--- src/helpers/effects.js
+++ src/helpers/effects.js
@@ -1,11 +1,10 @@
 // @flow
-import { partial } from 'ramda'
 import { maybe, type Maybe } from '@sbizeul/fp-flow'
 
-const getElementById = (document: any, id: string): Maybe<HTMLElement> => {
+const getElementById = (document: any) => (id: string): Maybe<HTMLElement> => {
   return maybe.fromNullable(document.getElementById(id))
 }
 
 export default {
-  getElementById: partial(getElementById, [window.document])
+  getElementById: getElementById(window.document)
 }
